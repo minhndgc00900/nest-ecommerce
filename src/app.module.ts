37bc1cfa52024a8f364efe8732abd6c2f220ebbb
Minh Nguyen <minhnd0496@gmail.com>
2022-07-07 10:19:30 +0700

    in progress

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,15 @@ import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
   imports: [
     ProductModule,
     MongooseModule.forRoot('mongodb://localhost/store'),
     UserModule,
-    AuthModule, // 1.2 Setup the database
+    AuthModule,
+    CartModule, // 1.2 Setup the database
   ],
   controllers: [AppController],
   providers: [AppService],
