48ccfde7070c17a77e649a3f61e6c9d8fa2692a6
Minh Nguyen <minhnd0496@gmail.com>
2022-07-07 11:56:40 +0700

    done create service for cart

--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -1,7 +1,94 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { ItemDTO } from './dtos/item.dto';
+import { Cart, CartDocument } from './schemas/cart.schema';
 
 @Injectable()
 export class CartService {
-  constructor(@Injec) {
+  constructor(
+    @InjectModel('Cart') private readonly cartModel: Model<CartDocument>,
+  ) {}
+
+  async createCart(
+    userId: string,
+    itemDTO: ItemDTO,
+    subTotalPrice: number,
+    totalPrice: number,
+  ): Promise<Cart> {
+    const newCart = await this.cartModel.create({
+      userId,
+      items: [
+        {
+          ...itemDTO,
+          subTotalPrice,
+        },
+      ],
+      totalPrice,
+    });
+
+    return newCart;
+  }
+
+  async getCart(userId: string): Promise<CartDocument> {
+    const cart = await this.cartModel.findOne({ userId });
+    return cart;
+  }
+
+  async deleteCart(userId: string): Promise<Cart> {
+    const deleteCart = await this.cartModel.findOneAndRemove({ userId });
+    return deleteCart;
+  }
+
+  async recalculateCart(cart: CartDocument) {
+    cart.totalPrice = 0;
+    cart.items.forEach((item) => {
+      cart.totalPrice += item.quantity * item.price;
+    });
+  }
+
+  async addItemToCart(userId: string, itemDTO: ItemDTO): Promise<Cart> {
+    const { productId, quantity, price } = itemDTO;
+    const subTotalPrice = quantity * price;
+
+    const cart = await this.getCart(userId);
+
+    if (cart) {
+      const itemIndex = cart.items.findIndex(
+        (item) => item.productId == productId,
+      );
+      if (itemIndex > -1) {
+        const item = cart.items[itemIndex];
+        item.quantity = Number(item.quantity) + Number(quantity);
+        item.subTotalPrice = item.quantity * item.price;
+        cart.items[itemIndex] = item;
+      } else {
+        cart.items.push({ ...itemDTO, subTotalPrice });
+      }
+      await this.recalculateCart(cart);
+      return cart.save();
+    } else {
+      const newCart = await this.createCart(
+        userId,
+        itemDTO,
+        subTotalPrice,
+        price,
+      );
+
+      return newCart;
+    }
+  }
+
+  async removeItemFromCart(userId: string, productId: string): Promise<any>{
+    const cart = await this.getCart(userId);
+
+    const itemIndex = cart.items.findIndex(
+      (item) => item.productId === productId,
+    );
+
+    if (itemIndex > -1) {
+      cart.items.splice(itemIndex, 1);
+      return cart.save();
+    }
   }
 }
